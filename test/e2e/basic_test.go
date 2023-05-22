bf6b37f1cb6eb72ab5f23eaaea0757a5a94a8c9c
Carlos Martins <carlos.martins@wildlifestudios.com>
2023-05-22 15:13:21 -0300

    unnecessary comment

--- test/e2e/basic_test.go
+++ test/e2e/basic_test.go
@@ -19,8 +19,6 @@ import (
 	"os"
 	"testing"
 	"time"
-	// "net/http"
-	// "io/ioutil"
 
 	api "github.com/coreos/etcd-operator/pkg/apis/etcd/v1beta2"
 	"github.com/coreos/etcd-operator/test/e2e/e2eutil"
